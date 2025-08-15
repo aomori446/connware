c99243d64c766376138cc0860914250744913f1f
aomori168 <aomori446@yahoo.co.jp>
2025-08-15 15:44:10 +0900

    a better speed limit model

--- speedlimit.go
+++ speedlimit.go
@@ -3,12 +3,11 @@ package connware
 import (
 	"context"
 	"io"
-	"time"
 
 	"golang.org/x/time/rate"
 )
 
-type DataSize float64
+type DataSize int
 
 const (
 	B  DataSize = 1
@@ -16,15 +15,6 @@ const (
 	MB DataSize = 1024 * KB
 	GB DataSize = 1024 * MB
 )
-
-type SpeedLimit struct {
-	Amount   DataSize
-	Interval time.Duration
-}
-
-func (s SpeedLimit) Limit() rate.Limit {
-	return rate.Limit(float64(s.Amount) / s.Interval.Seconds())
-}
 
 type SpeedLimitConn struct {
 	io.ReadWriteCloser
@@ -40,86 +30,41 @@ func NewSpeedLimitConn(ctx context.Context, conn io.ReadWriteCloser) *SpeedLimit
 	}
 }
 
-func (slc *SpeedLimitConn) SetReadLimit(speed SpeedLimit) *SpeedLimitConn {
-	slc.readLimit = rate.NewLimiter(speed.Limit(), int(speed.Amount))
-	return slc
-}
-
-func (slc *SpeedLimitConn) SetWriteLimit(speed SpeedLimit) *SpeedLimitConn {
-	slc.writeLimit = rate.NewLimiter(speed.Limit(), int(speed.Amount))
-	return slc
-}
-
 func (slc *SpeedLimitConn) Read(p []byte) (n int, err error) {
-	if slc.readLimit == nil {
-		return slc.ReadWriteCloser.Read(p)
+	l := len(p)
+	if slc.readLimit != nil {
+		l = min(len(p), slc.readLimit.Burst())
 	}
-
-	var totalRead int
-	for totalRead < len(p) {
-		toRead := len(p) - totalRead
-		burst := slc.readLimit.Burst()
-		if toRead > burst {
-			toRead = burst
-		}
-
-		if err := slc.readLimit.WaitN(slc.ctx, toRead); err != nil {
-			return totalRead, err
-		}
-
-		bytesRead, err := slc.ReadWriteCloser.Read(p[totalRead : totalRead+toRead])
-		if bytesRead > 0 {
-			totalRead += bytesRead
-		}
-
-		if err != nil {
-			return totalRead, err
-		}
+	err = slc.readLimit.WaitN(slc.ctx, l)
+	if err != nil {
+		return
 	}
-	return totalRead, nil
+	return slc.ReadWriteCloser.Read(p[:l])
 }
 
 func (slc *SpeedLimitConn) Write(p []byte) (n int, err error) {
-	if slc.writeLimit == nil {
-		return slc.ReadWriteCloser.Write(p)
+	l := len(p)
+	if slc.writeLimit != nil {
+		l = min(len(p), slc.writeLimit.Burst())
 	}
-
-	var totalWritten int
-	for totalWritten < len(p) {
-		toWrite := len(p) - totalWritten
-		burst := slc.writeLimit.Burst()
-		if toWrite > burst {
-			toWrite = burst
-		}
-
-		if err := slc.writeLimit.WaitN(slc.ctx, toWrite); err != nil {
-			return totalWritten, err
-		}
-
-		bytesWritten, err := slc.ReadWriteCloser.Write(p[totalWritten : totalWritten+toWrite])
-		if bytesWritten > 0 {
-			totalWritten += bytesWritten
-		}
-
-		if err != nil {
-			return totalWritten, err
-		}
+	err = slc.writeLimit.WaitN(slc.ctx, l)
+	if err != nil {
+		return
 	}
-
-	return totalWritten, nil
+	return slc.ReadWriteCloser.Write(p)
 }
 
 type SpeedLimitOption func(*SpeedLimitConn)
 
-func WithReadLimit(speed SpeedLimit) SpeedLimitOption {
+func WithReadLimit(size DataSize) SpeedLimitOption {
 	return func(slc *SpeedLimitConn) {
-		slc.readLimit = rate.NewLimiter(speed.Limit(), int(speed.Amount))
+		slc.readLimit = rate.NewLimiter(rate.Limit(size)-1, int(size))
 	}
 }
 
-func WithWriteLimit(speed SpeedLimit) SpeedLimitOption {
+func WithWriteLimit(size DataSize) SpeedLimitOption {
 	return func(slc *SpeedLimitConn) {
-		slc.writeLimit = rate.NewLimiter(speed.Limit(), int(speed.Amount))
+		slc.writeLimit = rate.NewLimiter(rate.Limit(size)-1, int(size))
 	}
 }
 
